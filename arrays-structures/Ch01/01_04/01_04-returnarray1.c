4980aeace63e69d26e4fa83bef2b3ed57e203872
root <root@DESKTOP-675FT6R.localdomain>
2020-09-06 14:38:31 -0400

    Adds information about arrays and how to return arrays from functions. Particularly using pointers, as it is not allowed to return an actual full array from a function. Adds information about structs, struct declarations, and typedef with structs (namespaces).

--- arrays-structures/Ch01/01_04/01_04-returnarray1.c
+++ arrays-structures/Ch01/01_04/01_04-returnarray1.c
@@ -2,26 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-int makearray(void)
+int *makearray(void)
 {
-	int array[5];
+
+	// int array[5] :: this will not work as memory is freed from the stack once this function is returned. As such, this variable will not be securely pointing at
+					// what you think it is pointing at
+	
+	// Using static will work as static local variables keep their memory cell throughout all program execution. 
+	static int array[5]; 
+
 	int x;
 
-	for(x=0;x<5;x++)
+	for(x = 0 ; x < 5; x++)
 		array[x] = rand() % 10 + 1;
 
-	return(array);
+	// return(array); This line will not work if the signature is not "int *makearray", as you cannot return an entire array from a function
+	/* You must return either: 
+		* the values individually (eg: calling a function many times that returns single elements)
+		* a pointer to the array
+	*/
+
+	return array;
 }
 
 int main()
 {
-	int x,r[5];
+	int x;
+	int *p;
 
 	srand( (unsigned)time(NULL) );
 	puts("Here are your 5 random numbers:");
-	r = makearray();
-	for(x=0;x<5;x++)
-		printf("%d\n",r[x]);
+	p = makearray();
+
+	for(x = 0; x < 5; x++)
+		printf("%d\n", p[x]);
 
 	return(0);
 }
